a7babd4db80ef176000543a1793c4be8e4f466ee
notibraa <sda_test@onet.pl>
2024-12-07 13:59:55 +0000

    Added artifact

--- src/train.py
+++ src/train.py
@@ -1,16 +1,21 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-
+from sklearn.linear_model import LinearRegression
+from joblib import dump
 
 df = pd.read_csv('data/otodom.csv')
 df = df.dropna()
 X = df[['area']]
-Y = df['price']
+y = df['price']
 X_train, X_test, y_train, y_test = train_test_split(X,
-                                                    Y,
+                                                    y,
                                                     test_size=0.2,
                                                     random_state=42)
 model = LinearRegression()
 model.fit(X_train, y_train)
-print(model.score(X_test, y_test))
+
+
+score = model.score(X_test, y_test)
+assert score > 0.7, 'Unsatisfied score'
+print('Model score:', score)
+dump(model, 'models/model.joblib')
